f0c51fced1c2a834b45056d157328573e06b3ecb
evan quessy <evanquessy@hotmail.com>
2016-10-28 14:18:43 -0400

    added a compteux = 666 *200

--- ProgJV_TP3/ProgJV_TP3/Program.cs
+++ ProgJV_TP3/ProgJV_TP3/Program.cs
@@ -10,11 +10,15 @@ namespace ProgJV_TP3
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("666");
+            }
             for (int compteur = 1; compteur <= 200; compteur++)
             {
-                Console.WriteLine("eE");
+                 Console.WriteLine("eE");
             }
             Console.ReadLine();
-        }
+       }
     }
 }
